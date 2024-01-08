3430f4cffc332f8de28aef08f2743fb81bd76035
mossguo <mossguo@tencent.com>
2024-01-08 20:14:02 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useForm, SubmitHandler } from "react-hook-form"
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+
+  console.log(watch("example")) // watch input value by passing the name of it
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +21,18 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* register your input into the hook by invoking the "register" function */}
+          <input defaultValue="test" {...register("example")} />
+
+          {/* include validation with required or other standard HTML validation rules */}
+          <input {...register("exampleRequired", { required: true })} />
+          {/* errors will return when field validation fails  */}
+          {errors.exampleRequired && <span>This field is required</span>}
+
+          <input type="submit" />
+        </form>
         <a
           className="App-link"
           href="https://reactjs.org"
